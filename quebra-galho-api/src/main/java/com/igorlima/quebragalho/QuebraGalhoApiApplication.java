b84089c86bfc7bbc091c3f015680fbebdcd65f31
Igor <Igor@DESKTOP-TJ2AREL>
2019-09-29 20:05:48 -0300

    first commit

---
+++ quebra-galho-api/src/main/java/com/igorlima/quebragalho/QuebraGalhoApiApplication.java
@@ -0,0 +1,13 @@
+package com.igorlima.quebragalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuebraGalhoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuebraGalhoApiApplication.class, args);
+	}
+
+}
